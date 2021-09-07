cedb2c1e7db381b2fb0860d523369b6b86820927
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-09-08 00:46:20 +0700

    variables

---
+++ index.js
@@ -0,0 +1,24 @@
+//document.getElementById("count-el").innerText=40;
+
+//stiore dara on variabel
+let count = 12;
+
+console.log(count);
+
+//console.log(myAge);
+/*
+//1 create varible my age
+let myAge = 21;
+//2 print it
+console.log(myAge);
+*/
+
+let myAge = 21;
+let humaDogRatio = 7;
+
+let myDogAge = myAge*humaDogRatio;
+
+console.log(myDogAge);
+
+
+
