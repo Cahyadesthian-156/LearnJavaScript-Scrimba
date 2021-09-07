e38fe9c2b96f2e30f642b414cf5412ea84180360
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-09-08 01:20:48 +0700

    btn added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //document.getElementById("count-el").innerText=40;
 
 //stiore dara on variabel
-let count = 12;
+// let count = 12;
 
-console.log(count);
+// console.log(count);
 
 //console.log(myAge);
 /*
@@ -13,12 +13,35 @@ let myAge = 21;
 console.log(myAge);
 */
 
-let myAge = 21;
-let humaDogRatio = 7;
+// let myAge = 21;
+// let humaDogRatio = 7;
 
-let myDogAge = myAge*humaDogRatio;
+// let myDogAge = myAge*humaDogRatio;
 
-console.log(myDogAge);
+// console.log(myDogAge);
+
+
+//let count = 0;
+
+let ygDiTombol = 0;
+function increment() {
+    //console.log("Buttonnya di click");
+    ygDiTombol += 1;
+    document.getElementById('count-el').innerText=ygDiTombol;
+}
+
+
+
+function count() {
+    console.log("6");
+    console.log("5");
+    console.log("4");
+    console.log("3");
+    console.log("2");
+    console.log("1");
+}
+
+count();
 
 
 
