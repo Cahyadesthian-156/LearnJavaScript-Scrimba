2cea27eb50678ffb664616dc30fb8d1ab738e975
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-09-15 13:04:16 +0700

    21 intro Array

--- 3_Blackjack-game/index.js
+++ 3_Blackjack-game/index.js
@@ -2,6 +2,8 @@
 let firstCard = 10
 let secondCard = 11
 
+let cards = [firstCard, secondCard]
+
 let sum = firstCard + secondCard
 //console.log(sum);
 
@@ -12,10 +14,23 @@ let isAlife = true;
 let message=""
 //let message = ""  //bisa gini juga
 
-var messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let messageEl = document.getElementById("message-el")
+//console.log(messageEl)
+
+let sumEl = document.getElementById("sum-el");
+//console.log(sumEl);
+
+let cardEl = document.getElementById("card-el");
+console.log(cardEl);
+
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+
+    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
 
     if(sum<=20) {
         message = "Wanna draw a new Card ? " 
@@ -29,15 +44,22 @@ function startGame() {
     
     //console.log(message);
     messageEl.textContent = message
-
+    sumEl.textContent = "Sum: " + sum
 }
-
-
 
 //console.log(hasBlackJack);
 //console.log(isAlife);
 
+function newCard() {
+    console.log("Drawing new card from the Deck!");
 
+    let card = 4
+
+    sum += card
+
+    renderGame()
+
+}
 
 
 
---
+++ 3_Blackjack-game/introArray/index.js
@@ -0,0 +1,16 @@
+///Array - ordered lists of items
+
+let featuredPost = [
+    "Checkout my Netflix clone",
+    "Here's the code for my project",
+    "I ve just relaunched my Portofolio"
+]
+
+let experience = [
+    "Hipster at some new things",
+    "Designer using Photoshop adn Figma",
+    "Informatics Bachelor",
+    "Dicoding Certification, UX Designer RuangGuru"
+]
+
+
