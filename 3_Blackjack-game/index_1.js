d3ffcb28b57f70e53c4b836f0753bbaa7948f65f
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-09-16 00:53:33 +0700

    logical operator 45

--- 3_Blackjack-game/index_1.js
+++ 3_Blackjack-game/index_1.js
@@ -1,12 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [
-    firstCard,
-    secondCard
-]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -31,6 +26,12 @@ function getRandomCard() {
 
 
 function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum = firstCard + secondCard
+    isAlive = true
     renderGame()
 }
 
---
+++ 3_Blackjack-game/logicalOperator/index.js
@@ -0,0 +1,11 @@
+let hasCompletedCourse = true
+let givesCertificate = true
+let cheating = false
+
+if (hasCompletedCourse === true && givesCertificate === true && cheating === false) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating Certificate");
+}
