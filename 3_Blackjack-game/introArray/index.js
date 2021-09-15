5b0479b9fd0164708159ed002f6410020a678530
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-09-15 18:38:01 +0700

    25 pop array

--- 3_Blackjack-game/introArray/index.js
+++ 3_Blackjack-game/introArray/index.js
@@ -6,11 +6,59 @@ let featuredPost = [
     "I ve just relaunched my Portofolio"
 ]
 
+/*
 let experience = [
     "Hipster at some new things",
     "Designer using Photoshop adn Figma",
     "Informatics Bachelor",
     "Dicoding Certification, UX Designer RuangGuru"
 ]
+*/
+
+let experience = [
+    "CEO at Scrimba",
+    "Frontend developer at Xeneta",
+    "People counter for Norstat"
+]
+
+console.log(featuredPost[2]);
+console.log(experience[1]);
+console.log(experience[2]);
+console.log(experience[0]);
+console.log(experience.length);
+
+
+
+//multiple data type in anoe array
+let cahya = [
+    "Cahyadesthian",
+    21,
+    false
+]
+
+
+//adding and removing from array
+let cardsInHand = [7,4]
+cardsInHand.push("ace")
+console.log(cardsInHand);
+
+
+//push the newMessage ti message array and than log out the array
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good, Been working on my portofolio lately."
+]
+
+let newMessage = "Same here!"
+
+messages.push(newMessage);
+console.log(messages);
+
+//how can you remove last item in Array? try google it
+//done, find the anwer lets write it
+messages.pop()
+console.log(messages);
+
 
 
