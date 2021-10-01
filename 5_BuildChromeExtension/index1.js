4d607787938b2de0827a1f1a43a0bf3834c289bc
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-10-02 00:33:12 +0700

    ini template string

--- 5_BuildChromeExtension/index1.js
+++ 5_BuildChromeExtension/index1.js
@@ -9,6 +9,7 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
@@ -17,11 +18,18 @@ function renderLeads() {
     let listItem = ""
 
     for(let i=0; i< myLeads.length; i++) {
-        listItem += "<li>" + myLeads[i] + "</li>"
+        
+        listItem += `
+            <li>
+                <a href="${myLeads[i]}" target="_blank">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+
     }
 
     ulEl.innerHTML = listItem
-
 
 }
 
