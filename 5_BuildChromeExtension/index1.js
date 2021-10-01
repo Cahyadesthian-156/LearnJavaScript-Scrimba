898b1abc23324b01afde35e8c92fcd15581d4cbe
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-10-02 00:17:13 +0700

    btn input.value added

--- 5_BuildChromeExtension/index1.js
+++ 5_BuildChromeExtension/index1.js
@@ -9,19 +9,20 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    renderLeads()
 })
 
-//1 create variable, listItem, hold all html for list item, assign it to an empty string to begin with
-let listItem = ""
+function renderLeads() {
 
-for(let i=0; i< myLeads.length; i++) {
-        
-    //2 add the item to the listItem variabel instead of the ulEl .innerHTML
-    listItem += "<li>" + myLeads[i] + "</li>"
-    console.log(listItem);
+    let listItem = ""
+
+    for(let i=0; i< myLeads.length; i++) {
+        listItem += "<li>" + myLeads[i] + "</li>"
+    }
+
+    ulEl.innerHTML = listItem
+
+
 }
 
-//3 Render the listItem inside the unordered list using ulEl.inenrHTML
-ulEl.innerHTML = listItem
 
