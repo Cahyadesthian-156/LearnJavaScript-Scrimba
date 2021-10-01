3c256305e0e319a45c8016e85372fd7ffea14f37
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-10-02 00:42:47 +0700

    templateString/literal

---
+++ 5_BuildChromeExtension/4_firstTemplateString/index.js
@@ -0,0 +1,14 @@
+//template string
+
+const recipent = "Caty"
+const sender = "Widigda"
+
+//const email = "Hey " + recipent + "! How is it going? Cheers Desthian"
+
+const email = `
+    "Hey ${recipent}! 
+    How is it going? Nice right?
+    Cheers ${sender}"
+`
+
+console.log(email);
