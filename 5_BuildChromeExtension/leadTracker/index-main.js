d2518394d52f89c45aef5fb66b7fff9e927443e0
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-10-11 16:05:09 +0700

    local storage , next Storing arrays in local arrays

--- 5_BuildChromeExtension/leadTracker/index-main.js
+++ 5_BuildChromeExtension/leadTracker/index-main.js
@@ -3,6 +3,31 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
+// chalange
+/**
+ * 
+ * 1. save akey-value pair in local storage
+ * 2. refresh the page. get the value and log it to the console
+ * 3. clear localStorage
+ * 
+ * HINTS
+ * localStorage.setItem(key,value)
+ * localStorage.getItem(key)
+ * localStorage.clear()
+ * 
+ */
+
+//localStorage.setItem("cahyadesthian", "learning")
+let activity = localStorage.getItem("cahyadesthian")
+console.log(activity);
+
+localStorage.clear()
+
+console.log(localStorage.getItem("Cahyadesthia"));
+//console.log(check);
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
